2169a9442b0bc30cfe195fe1cfebef431176ba29
amuhammed005 <amuhammed005@st.ug.edu.gh>
2025-01-20 22:48:24 +0000

    Add header component and update layout structure

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 flex justify-between items-center">
+      <ul className="flex gap-4">
+        <li>
+          <Link href={"/sign-in"}>Sign in</Link>
+        </li>
+        <li className="hidden sm:block">
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li className="hidden sm:block">
+          <Link href={"/about"}>About</Link>
+        </li>
+      </ul>
+      <Link href={"/"} className="flex items-center gap-1">
+        <span className="text-2xl font-bold py-1 px-2 bg-amber-500 rounded-lg">
+          IMDd
+        </span>
+        <span className="text-xl hidden sm:inline">Clone</span>
+      </Link>
+    </div>
+  );
+}
